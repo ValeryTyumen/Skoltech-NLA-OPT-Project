e9e727714282641f50df30058ce765ed6f07b6ed
Aliaksandra Shysheya <afternonsister@gmail.com>
2017-12-18 14:52:28 +0300

    correct type in sparsing reg

--- sparsing_regularizer.py
+++ sparsing_regularizer.py
@@ -7,7 +7,7 @@ class SparsingRegularizer(BaseRegularizer):
     """Sparsing regularization for the model."""
 
     def __init__(self,  beta_0, alpha_0, beta, alpha, num_topics, num_words, num_docs):
-        self._smothing_reg = SmoothingRegularizer(beta_0=-beta_0, alpha_0=-alpha_0, beta=beta,
+        self._smoothing_reg = SmoothingRegularizer(beta_0=-beta_0, alpha_0=-alpha_0, beta=beta,
                                                   alpha=alpha, num_topics=num_topics,
                                                   num_words=num_words, num_docs=num_docs)
 
@@ -18,7 +18,7 @@ class SparsingRegularizer(BaseRegularizer):
         :param topic_in_doc_probs: numpy.ndarray
         :return: float, reg value
         """
-        return self._smothing_reg.get_value(word_in_topic_probs=word_in_topic_probs,
+        return self._smoothing_reg.get_value(word_in_topic_probs=word_in_topic_probs,
                                             topic_in_doc_probs=topic_in_doc_probs)
 
     def get_gradient(self, word_in_topic_probs, topic_in_doc_probs):
@@ -28,5 +28,5 @@ class SparsingRegularizer(BaseRegularizer):
         :param topic_in_doc_probs: numpy.ndarray
         :return: gradients
         """
-        return self._smothing_reg.get_gradient(word_in_topic_probs=word_in_topic_probs,
+        return self._smoothing_reg.get_gradient(word_in_topic_probs=word_in_topic_probs,
                                                topic_in_doc_probs=topic_in_doc_probs)
