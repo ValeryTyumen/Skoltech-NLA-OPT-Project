17f65d1d87cab23db0ee579fb915b9fb850dd81e
Marina Gomtsian <marina@Marinas-MacBook-Pro-2.local>
2017-12-19 10:57:41 +0300

    add elimination of insignificant topics regularizer

--- elimination_of_insignificant_topics.py
+++ elimination_of_insignificant_topics.py
@@ -4,10 +4,12 @@ import numpy as np
 class EliminationOfInsignificantTopics(BaseRegularizer):
     """Elimination of insignificant topics."""
     
-    def __init__(self, tau, num_docs, word_in_topic_freq, topic_in_doc_freq):
+    def __init__(self, tau, num_topics, num_words, num_docs, word_in_topic_freq, topic_in_doc_freq):
         """
         Initialize Regularizer.
         :param tau: float, parameter for regularizer
+        :param num_topics: int, number of topics in the model
+        :param num_words: int, vocab size
         :param num_docs: int, number of documents
         :param word_in_topic_freq: numpy.ndarray, frequencies of words in topics
         :param topic_in_doc_freq: numpy.ndarray, frequencies of topics in documents
@@ -17,10 +19,11 @@ class EliminationOfInsignificantTopics(BaseRegularizer):
         self._num_docs = num_docs
         self._word_in_topic_freq = word_in_topic_freq
         self._topic_in_doc_freq = topic_in_doc_freq
+        self._word_in_topic_buffer = np.zeros(shape=(num_words, num_topics))
         self._topic_in_doc_buffer = np.zeros(shape=(num_topics, num_docs))
         self._epsilon = 1e-10
         
-    def get_value(self, topic_in_doc_probs):
+    def get_value(self, word_in_topic_probs, topic_in_doc_probs):
         """
         Return value of regularizer.
         :param topic_in_doc_probs: numpy.ndarray
@@ -33,14 +36,16 @@ class EliminationOfInsignificantTopics(BaseRegularizer):
         regularization_eliminate_topic = tau * np.log(self._topic_in_doc_buffer).sum()
         return regularization_eliminate_topic
     
-    def get_gradient(self, topic_in_doc_probs):
+    def get_gradient(self, word_in_topic_probs, topic_in_doc_probs):
         """
         Compute gradient of regularizer w.r.t model parameters
+        :param word_in_topic_probs: numpy.ndarray
         :param topic_in_doc_probs: numpy.ndarray
         :return: gradient
         """
         np.copyto(self._topic_in_doc_buffer, topic_in_doc_probs)
         self._topic_in_doc_buffer[self._topic_in_doc_buffer == 0] = self._epsilon
         self._topic_in_doc_buffer[:, :] = \
-        self._tau * self._topic_in_doc_freq.sum(axis = 1) / self._word_in_topic_freq.sum()
-        return self._topic_in_doc_buffer
+        self._tau * np.repeat((self._topic_in_doc_freq.sum(axis = 0))[:, np.newaxis], 
+                              repeats = self._num_topics, axis = 1) / self._word_in_topic_freq.sum(axis = 0)
+        return self._word_in_topic_buffer, self._topic_in_doc_buffer
